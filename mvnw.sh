e997c1f515757d0a864e7bb7f2884d4d611d1148
anirban das <anirbanstifler@gmail.com>
2021-08-23 16:54:43 +0530

    updated docker compose instructions, fixed dockerfile configuration of each service for building docker image, removed unused configurations, fixed operations shell script of project

--- mvnw.sh
+++ mvnw.sh
@@ -1,13 +1,7 @@
-mvn clean package -DskipTests -e
-
-mvn compile jib:build -e
-
-# mvn clean package -e
-
-# mvn spring-boot:run -Ddockerfile.skip -e
-
-# mvn surefire-report:report -e
-
-# mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -DskipTests -Ddockerfile.skip -e
-
-# hoverfly.exe -pp 1080 -webserver
+mvn clean package -DskipTests -e \
+&& mvn compile com.google.cloud.tools:jib-maven-plugin:build -DskipTests -e \
+# mvn clean package -e \
+# mvn spring-boot:run -Ddockerfile.skip -e \
+# mvn surefire-report:report -e \
+# mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -DskipTests -Ddockerfile.skip -e \
+# hoverfly.exe -pp 1080 -webserver \
